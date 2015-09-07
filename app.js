765a5d96e343f659f559626c41d4c17bb7b3e7ee
Jack Perkins <jackaperkins@gmail.com>
2015-09-06 21:20:36 -0700

    Add some good console.logs

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var http = require('http');
 
 var server = http.createServer(function (request, response) {
   // the incomming request object has all the info we
+  console.log("incomming request for " + request.url);
 
   wolfram.query("integrate 2x", function (err, data) {
+
     if (err) throw err;
-    console.log("Result: %j", data);
+    console.log("Wolfram responded!");
 
     // we need to write some data back to the http response
     // set the header so browsers know this is json
@@ -17,3 +19,4 @@ var server = http.createServer(function (request, response) {
 });
 
 server.listen(8080);
+console.log("server listening on localhost:8080");
