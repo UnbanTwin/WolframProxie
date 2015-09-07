c171a05c4d6daacd9e7ca845c4b7a5a861bf09f2
Jack Perkins <jackaperkins@gmail.com>
2015-09-06 21:17:00 -0700

    Add bare-bones http server

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var wolfram = require('wolfram-alpha').createClient("THYQLJ-3K45Y2A7W5");
+var http = require('http');
 
-wolfram.query("integrate 2x", function (err, result) {
-  if (err) throw err;
-  console.log("Result: %j", result);
+var server = http.createServer(function (request, response) {
+  // the incomming request object has all the info we
+
+  wolfram.query("integrate 2x", function (err, data) {
+    if (err) throw err;
+    console.log("Result: %j", data);
+
+    // we need to write some data back to the http response
+    // set the header so browsers know this is json
+    response.setHeader("Content-Type", "application/json");
+    response.write(JSON.stringify(data));
+    response.end();
+  });
 });
+
+server.listen(8080);
