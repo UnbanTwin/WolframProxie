9634e6a1a12b7ad806840424223da9f6157e0613
wizardsambolton <sam99bolton@gmail.com>
2015-11-04 21:29:11 +0000

    added dice route

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ var server = http.createServer(function (request, response) {
         console.dir(sides);
         if (isNaN(sides)) sides = 6;
         var number = Math.floor(Math.random() * sides + 1);
+        response.setHeader("Content-Type", "application/json");
+        response.setHeader('Access-Control-Allow-Origin', '*');
         response.write(JSON.stringify({result : number}));
+
         response.end()
 
     }else {
