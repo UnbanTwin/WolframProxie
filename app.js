79b807ff6a534015f8d8113294665aa46d8b9d57
wizardsambolton <sam99bolton@gmail.com>
2015-11-04 20:51:58 +0000

    added multiple routes

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ var http = require('http');
 var url = require('url');
 
 var server = http.createServer(function (request, response) {
-  if (request.url.indexOf("favicon") != -1) return response.end();
-  // the incomming request object has all the info we
-  console.log("incomming request for " + request.url);
-  console.log(url.parse(request.url, true));
-  var search = url.parse(request.url, true).query.search;
-  wolfram.query(search, function (err, data) {
+    if (request.url.indexOf("favicon") != -1) return response.end();
+    if (request.url.indexOf("wolfram") != -1) {
+        console.log("incomming request for " + request.url);
+        console.log(url.parse(request.url, true));
+        var search = url.parse(request.url, true).query.search;
+        wolfram.query(search, function (err, data) {
 
-    if (err) throw err;
-    console.log("Wolfram responded!");
+            if (err) throw err;
+            console.log("Wolfram responded!");
 
-    // we need to write some data back to the http response
-    // set the header so browsers know this is json
-    response.setHeader("Content-Type", "application/json");
-    response.setHeader('Access-Control-Allow-Origin', '*');
-    response.write(JSON.stringify(data));
-    response.end();
-  });
+            // we need to write some data back to the http response
+            // set the header so browsers know this is json
+            response.setHeader("Content-Type", "application/json");
+            response.setHeader('Access-Control-Allow-Origin', '*');
+            response.write(JSON.stringify(data));
+            response.end();
+            return
+        });
+    }else {
+        response.end();
+    }
+
+    // the incomming request object has all the info we
+
 });
 
 server.listen(process.env.PORT || 8080);
